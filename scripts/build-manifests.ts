d9410b2f18e5d8fc2bf26b47b01721991fadfb21
Simeon Radivoev <simeonradivoev@gmail.com>
2026-05-15 22:52:58 +0300

    build: Added game manifest building

--- scripts/build-manifests.ts
+++ scripts/build-manifests.ts
@@ -2,11 +2,15 @@ import { glob, readFile, writeFile, mkdir } from "fs/promises";
 import { existsSync } from "fs";
 
 const games: string[] = [];
-for await (const file of glob("./buckets/games/*.json")) {
+for await (const file of glob("./buckets/games/*.json"))
+{
   games.push(file);
 }
 const emulatorsManifest: any[] = [];
-for await (const file of glob("./buckets/emulators/*.json")) {
+const gamesManifest: any[] = [];
+
+for await (const file of glob("./buckets/emulators/*.json"))
+{
   const { name, logo, homepage, os } = JSON.parse(await readFile(file, "utf8"));
   emulatorsManifest.push({
     name,
@@ -21,19 +25,31 @@ const playerCountSet = new Set<string>();
 const companiesSet = new Set<string>();
 let gameCount = 0;
 
-for await (const file of games) {
+for await (const file of games)
+{
   const json = JSON.parse(await readFile(file, "utf8"));
+  gamesManifest.push({
+    name: json.name,
+    homepage: json.homepage,
+    version: json.version,
+    covers: json.covers,
+    igdb_id: json.igdb_id
+  });
   gameCount++;
-  if (json.genres) {
+  if (json.genres)
+  {
     json.genres.forEach((g: string) => genresSet.add(g));
   }
-  if (json.keywords) {
+  if (json.keywords)
+  {
     json.keywords.forEach((k: string) => keywordsSet.add(k));
   }
-  if (json.player_count) {
+  if (json.player_count)
+  {
     playerCountSet.add(json.player_count);
   }
-  if (json.companies) {
+  if (json.companies)
+  {
     json.companies.forEach((c: string) => companiesSet.add(c));
   }
 }
@@ -44,6 +60,13 @@ await writeFile(
   "./manifests/emulators.json",
   JSON.stringify({
     emulators: emulatorsManifest,
+  }),
+);
+
+await writeFile(
+  "./manifests/games.json",
+  JSON.stringify({
+    emulators: gamesManifest,
   }),
 );
 
